73fbb06a9ed78cbcdff5eb79f3112664317cf500
hilderonny <ronny.hildebrandt@avorium.de>
2017-03-25 20:14:22 +0000

    Toll zur SPrachausgabe eingebaut

---
+++ pi1/speak.py
@@ -0,0 +1,16 @@
+# Modul und Applikation zur Sprachausgabe
+# Applikation verwenden: python speak.py "Hallo Welt!"
+# Modul verwenden:
+# from speak import *
+# speak('Hallo Welt!')
+import subprocess
+
+def speak(text):
+	subprocess.call(['pico2wave', '-l', 'de-DE', '-w', 'temp.wav', text])
+	subprocess.call(['aplay', 'temp.wav'])
+	subprocess.call(['rm', 'temp.wav'])
+	print text
+
+if __name__ == '__main__':
+	import sys
+	speak(sys.argv[1])
