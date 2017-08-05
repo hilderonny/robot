285df9a2260e4309c23f21938820c41d8402e927
hilderonny <ronny.hildebrandt@avorium.de>
2017-08-05 21:48:23 +0000

    Proxy hat jetzt auch lokalen Webserver.

--- pi1/pi1.js
+++ pi1/pi1.js
@@ -1,5 +1,30 @@
-var http = require('http');
+/*
+// Siehe https://www.npmjs.com/package/express-http-proxy
+var proxy = require('express-http-proxy');
+var express = require('express');
+
+var app = express();
+ 
+app.use('/pi2', proxy('10.0.0.2'));
+app.use('/pi3', proxy('10.0.0.3'));
+app.use('/pi4', proxy('10.0.0.4'));
+app.use('/pi5', proxy('10.0.0.5'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(80, function () {
+  console.log('Läuft');
+});
+*/
+
+// Siehe https://github.com/nodejitsu/node-http-proxy/blob/master/lib/http-proxy.js#L22-L50
+var express = require('express');
 var httpProxy = require('http-proxy');
 
-httpProxy.createProxyServer({target:'http://10.0.0.2:80'}).listen(80);
+httpProxy.createProxyServer({target:'http://10.0.0.2:80'}).listen(82);
 
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.listen(80, function () {
+  console.log('Läuft');
+});
