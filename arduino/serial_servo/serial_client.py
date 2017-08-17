bee86713f0641a4fe0221fa9adf31a902786b02c
hilderonny <gitlab@hildebrandt2014.de>
2017-08-17 22:11:10 +0200

    Test für serielle Servosteuerung eingebaut.

---
+++ arduino/serial_servo/serial_client.py
@@ -0,0 +1,25 @@
+# -*- coding: iso-8859-1 -*-
+'''
+Kommuniziert mit serial_servo.ino, welches auf Arduino installiert sein muss.
+Lässt den Servo an Pin 9 zwischen 70 und 110 Grad schwenken.
+
+Siehe: https://playground.arduino.cc/Interfacing/Python
+
+Installation:
+pip install pyserial
+'''
+import serial
+import time
+import struct
+
+serialPort = 'COM4'
+baudRate = 9600
+
+serialConnection = serial.Serial(serialPort, baudRate, timeout=1)
+time.sleep(2) # Auf Auto-Reset warten (https://playground.arduino.cc/Main/DisablingAutoResetOnSerialConnection)
+serialConnection.write(chr(70))
+time.sleep(2)
+serialConnection.write(chr(110))
+time.sleep(2)
+serialConnection.write(chr(90))
+serialConnection.close()
