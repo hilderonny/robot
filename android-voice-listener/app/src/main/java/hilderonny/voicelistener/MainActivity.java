c2b931321494b195c6cb93e779859f460f30c9ad
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2018-03-19 14:15:43 +0100

    UI cleanup. Endless hear-listen-loop implemented.

--- android-voice-listener/app/src/main/java/hilderonny/voicelistener/MainActivity.java
+++ android-voice-listener/app/src/main/java/hilderonny/voicelistener/MainActivity.java
@@ -3,6 +3,9 @@ package hilderonny.voicelistener;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.os.Handler;
+import android.os.Looper;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
@@ -11,12 +14,10 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.CompoundButton;
-import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 
 /**
@@ -31,24 +32,24 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity implements RecognitionListener {
 
     private static final int REQUEST_RECORD_PERMISSION = 100;
-    private TextView returnedText;
-    private ToggleButton toggleButton;
     private SpeechRecognizer speech = null;
     private Intent recognizerIntent;
     private TextToSpeech tts;
 
+    // Speak text and trigger callback onUtteranceCompleted() when finished, see https://stackoverflow.com/a/4659154/5964970
     private void speak(String text) {
-        returnedText.setText(text);
-        if (tts != null) tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        if (tts != null) {
+            HashMap<String, String> myHashAlarm = new HashMap<>();
+            myHashAlarm.put(TextToSpeech.Engine.KEY_PARAM_STREAM, String.valueOf(AudioManager.STREAM_ALARM));
+            myHashAlarm.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "READY SPOKEN");
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, myHashAlarm);
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        returnedText = findViewById(R.id.textView1);
-        toggleButton = findViewById(R.id.toggleButton1);
 
         speech = SpeechRecognizer.createSpeechRecognizer(this);
         speech.setRecognitionListener(this);
@@ -58,23 +59,26 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
 
-        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if (isChecked) {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_PERMISSION);
-                } else {
-                    speech.stopListening();
-                }
-            }
-        });
-
         tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                if (status != TextToSpeech.ERROR){
+                if (status == TextToSpeech.SUCCESS){
+                    // At initialization request permissions. This triggers speaking "Bin bereit" which causes onUtteranceCompleted() to be called which starts listening
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_PERMISSION);
                     tts.setLanguage(Locale.GERMAN);
-                    speak("Sprachausgabe bereit.");
+                    // Callback after finished speaking. Start listening immediately
+                    tts.setOnUtteranceCompletedListener(new TextToSpeech.OnUtteranceCompletedListener() {
+                        @Override
+                        public void onUtteranceCompleted(String s) {
+                            // Run in main thread, see https://stackoverflow.com/a/48891290/5964970
+                            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    speech.startListening(recognizerIntent);
+                                }
+                            });
+                        }
+                    });
                 }
             }
         });
@@ -85,7 +89,8 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_RECORD_PERMISSION) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                speech.startListening(recognizerIntent);
+                // Trigger speak-hear-loop
+                speak("Bin bereit.");
             } else {
                 Toast.makeText(MainActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
             }
@@ -99,21 +104,18 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
     }
 
     @Override
-    public void onEndOfSpeech() {
-        toggleButton.setChecked(false);
-    }
-
-    @Override
     public void onResults(Bundle results) {
         ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
         if (matches != null && matches.size() > 0) {
             String text = matches.get(0);
+            // Speak out listened text which triggers re-listening
             speak(text);
         }
     }
 
     @Override public void onBeginningOfSpeech() { }
     @Override public void onBufferReceived(byte[] bytes) { }
+    @Override public void onEndOfSpeech() { }
     @Override public void onError(int i) { }
     @Override public void onEvent(int i, Bundle bundle) { }
     @Override public void onPartialResults(Bundle bundle) { }
