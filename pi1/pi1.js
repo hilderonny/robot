30752ec0d61797aa6dc592ce8752a4f881a1f12b
hilderonny <ronny.hildebrandt@levelupsoftware.de>
2019-10-10 21:03:44 +0200

    Laptop zu Pi1 migriert.

--- pi1/pi1.js
+++ pi1/pi1.js
@@ -1,68 +1,20 @@
-/*
-// Siehe https://www.npmjs.com/package/express-http-proxy
-var proxy = require('express-http-proxy');
 var express = require('express');
-
-var app = express();
- 
-app.use(express.static(__dirname + '/public'));
-
-app.listen(80, function () {
-  console.log('Läuft');
-});
-*/
-
-// Siehe https://github.com/nodejitsu/node-http-proxy/blob/master/lib/http-proxy.js#L22-L50
-var express = require('express');
-var httpProxy = require('http-proxy');
 var https = require('https');
 var fs = require('fs');
+var webrtc = require('./webrtc');
 
-/*httpProxy.createProxyServer({target:'http://10.0.0.2:80'}).listen(82);
-httpProxy.createProxyServer({target:'http://10.0.0.3:80'}).listen(83);
-httpProxy.createProxyServer({target:'http://10.0.0.4:80'}).listen(84);
-httpProxy.createProxyServer({target:'http://10.0.0.5:80'}).listen(85);
-*/
-
-var proxy = httpProxy.createProxyServer({});
-
+// Express application
 var app = express();
-
-// Redirect to https, see https://www.tonyerwin.com/2014/09/redirecting-http-to-https-with-nodejs.html
-app.enable('trust proxy');
-app.use (function (req, res, next) {
-  if (req.secure) {
-    next();
-  } else {
-    res.redirect('https://' + req.headers.host + req.url);
-  }
-});
-var options = {
-  key: fs.readFileSync('/etc/ssl/private/selfsigned.key'),
-  cert: fs.readFileSync('/etc/ssl/certs/selfsigned.crt')
-};
-https.createServer(options, app).listen(443);
-
 app.use(express.static(__dirname + '/public'));
 
-app.get('/pi/:id', function(req, res) {
-  proxy.web(req, res, { ignorePath: true, target: `http://10.0.0.${req.params.id}:80/video_feed` });
+// HTTPS server
+var options = {
+  key: fs.readFileSync('ssl.key'),
+  cert: fs.readFileSync('ssl.crt')
+};
+var server = https.createServer(options, app).listen(443, function() {
+  console.log('Running.');
 });
 
-app.get('/pi1', function(req, res) {
-  proxy.web(req, res, { ignorePath: true, target: `http://10.0.0.1:81/video_feed` });
-});
-
-app.get('/pi4/:port/:value', function(req, res) {
-  proxy.web(req, res, { ignorePath: true, target: `http://10.0.0.4/move/${req.params.port}/${req.params.value}` });
-});
-
-
-app.get('/odroid/video0', function(req, res) {
-  proxy.web(req, res, { ignorePath: true, target: `http://10.0.0.2:8080` });
-});
-
-
-app.listen(80, function () {
-  console.log('Running');
-});
+// WebRTC server
+webrtc(server);
