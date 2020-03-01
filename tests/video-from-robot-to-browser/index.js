594ccf792ee8e4c8e42738851f542e56fa9aecb4
Ronny Hildebrandt <hilderonny2014@gmail.com>
2020-03-01 16:31:47 +0000

    jpeg test eingebaut

--- tests/video-from-robot-to-browser/index.js
+++ tests/video-from-robot-to-browser/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var https = require('https');
 var fs = require('fs');
-var binaryjs = require('binaryjs');
 var spawn = require('child_process').spawn;
-var PassThrough = require('stream').PassThrough;
+var P2J = require('pipe2jpeg');
+
+const params = [
+    /* log info to console */
+    '-loglevel',
+    'quiet',
+
+    /* use an artificial video input */
+    '-re',
+    '-f',
+    'lavfi',
+    '-i',
+    'testsrc=size=1920x1080:rate=15',
+
+    /* set output flags */
+    '-an',
+    '-c:v',
+    'mjpeg',
+    '-pix_fmt',
+    'yuvj422p',
+    '-f',
+    'image2pipe',//image2pipe, singlejpeg, mjpeg, or mpjpeg
+    '-vf',
+    'fps=1,scale=640:360',
+    '-q',
+    '1',
+    '-frames',
+    '100',
+    'pipe:1'
+];
 
 // Express application
 var app = express();
@@ -11,24 +39,26 @@ app.use(express.static(__dirname + '/public'));
 
 // HTTPS server
 var options = {
-    key: fs.readFileSync('ssl.key'),
-    cert: fs.readFileSync('ssl.crt')
+  key: fs.readFileSync('ssl.key'),
+  cert: fs.readFileSync('ssl.crt')
 };
 var server = https.createServer(options, app);
 server.listen(443, function() {
-    console.log('Running.');
+  console.log('Running.');
 });
 
-var ps;
+var p2j = new P2J();
+var lastImage;
 
-app.get('/stream', function(req, res) {
-    if (ps) {
-        console.log('Killing previous recording');
-        ps.kill();
-    }
-    console.log("Stream Start");
-    res.set({'Content-Type':'audio/wav'});
-    ps = spawn('arecord', ['-D', 'dmic_sv', '-c2', '-r', '48000', '-f', 'S32_LE', '-t', 'wav', '-V', 'stereo']);
-    ps.stderr.pipe(process.stdout);
-    ps.stdout.pipe(res);
+p2j.on('jpeg', function(jpeg) {
+  console.log('Bild');
+  lastImage = jpeg;
+});
+
+const ffmpeg = spawn('ffmpeg', params, {stdio : ['ignore', 'pipe', 'ignore']});
+ffmpeg.stdout.pipe(p2j);
+
+app.get('/stillimage', function(req, res) {
+  res.set({'Content-Type':'image/jpeg'});
+  res.send(lastImage);
 });
