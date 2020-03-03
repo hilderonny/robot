556392c78fca2f1f1b12fb80d6f1669f6591b386
Ronny Hildebrandt <hilderonny2014@gmail.com>
2020-03-03 19:48:54 +0000

    800x600x50% optimiert

--- pi2/camserver.py
+++ pi2/camserver.py
@@ -1,18 +1,28 @@
 import cv2
 from flask import Flask, render_template, Response
+import sys
 
 class VideoCamera(object):
+
+
     def __init__(self):
+        self.w = 800
+        self.h = 600
+        self.lp = 134
+        self.tp = 157
+        self.rp = self.w - 175
+        self.bp = self.h - 108
         self.video = cv2.VideoCapture(-1)
-        self.video.set(3,640)
-        self.video.set(4,480)
+        self.video.set(3,self.w)
+        self.video.set(4,self.h)
     
     def __del__(self):
         self.video.release()
     
     def get_frame(self):
         success, image = self.video.read()
-        ret, jpeg = cv2.imencode('.jpg', image, [cv2.IMWRITE_JPEG_QUALITY, 25])
+        cropped = image[self.tp:self.bp, self.lp:self.rp]
+        ret, jpeg = cv2.imencode('.jpg', cropped, [cv2.IMWRITE_JPEG_QUALITY, 50])
         return jpeg.tostring()
 
 app = Flask(__name__)
@@ -37,4 +47,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True,port=80)
+    app.run(host='0.0.0.0', debug=True,port=8080)
